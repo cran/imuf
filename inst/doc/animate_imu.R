7be7f406fb081bf7681b157e1e4b431dae812927
Felix Chan <chanfelix@gmail.com>
2025-01-12 00:30:02 +0000

    version 0.5.0

---
+++ inst/doc/animate_imu.R
@@ -0,0 +1,49 @@
+## ----include = FALSE----------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ----setup, message = FALSE---------------------------------------------------
+library(imuf)
+library(purrr)
+#
+lst_ned_in <- as.list(as.data.frame(t(walking_shin_1))) %>% unname
+dt <- 1/50
+#
+myCompUpdate <- function(initQ, accgyr) {
+  acc <- accgyr[1:3]
+  gyr <- accgyr[4:6]
+  gain <- 0.1
+  orientation <- compUpdate(acc, gyr, dt, initQ, gain)
+  orientation
+}
+#
+orientations <- purrr::accumulate(lst_ned_in, myCompUpdate, .init = c(1, 0, 0, 0))
+#
+head(orientations)
+
+## -----------------------------------------------------------------------------
+#
+# time increment for animation needs to be in milli-seconds
+animate_imu(orientations, dt*1000)
+
+## -----------------------------------------------------------------------------
+library(shiny)
+
+ui = pageWithSidebar(
+  headerPanel("render an IMU animation example"),
+  sidebarPanel(actionButton("do", "See animation")),
+  mainPanel(animate_imuOutput('orientations'))
+)
+
+server = function(input, output, session) {
+  observeEvent(input$do, {
+    output$orientations <- renderAnimate_imu(
+      animate_imu(orientations, dt*1000)
+    )
+  })
+}
+
+shinyApp(ui = ui, server = server)
+
